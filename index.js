d7ceb9a1d56d6221527c7080646bd7c27626ed48
Ben Sullivan <bensully513@gmail.com>
2018-04-10 11:56:26 -0600

    adding to heroku

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const instructors = require('./instructors')
+const app = express()
+const port = process.env.PORT || 3040
+const cors = require('cors')
+
+app.use(cors())
+
+let selectedId = (instructors, id) => {
+    for (let i = 0; i < instructors.length; i++) {
+        if (instructors[i]["ID"] == id) {
+            return instructors[i]
+        }
+    }
+}
+
+app.get('/', (req, res, next) => {
+    res.json(instructors)
+    next()
+})
+
+app.get('/:id', (req, res, next) => {
+    let instructorInfo = selectedId(instructors, req.params.id)
+    if (instructorInfo == null) {
+        res.send('Error: Invalid ID')
+    }
+    else {
+    res.json({data: instructorInfo})
+    }
+})
+
+app.listen(port, () => {console.log(`listening on port ${port}`)})
